005edbe078141a6c0eec44a00f522c80f1e42922
Anmol Upadhyay <99966995+anmolupadhyay23@users.noreply.github.com>
2023-12-02 10:18:47 +0530

    first commit

---
+++ lib/api_keys.dart
@@ -0,0 +1,2 @@
+const openAI_ApiKey='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';
+
---
+++ lib/feature_box.dart
@@ -0,0 +1,55 @@
+import 'package:allen/pallete.dart';
+import 'package:flutter/material.dart';
+
+class FeatureBox extends StatelessWidget {
+
+  final Color color;
+  final String headerText;
+  final String description;
+
+  const FeatureBox({Key? key, required this.color, required this.headerText, required this.description}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 35,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(Radius.circular(15))
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20,left: 15,bottom: 20),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                headerText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            const SizedBox(height: 3,),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text(
+                description,
+                style: const TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.blackColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
